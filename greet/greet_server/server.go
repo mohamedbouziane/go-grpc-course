51d2b5ea7f985e14ef22df5138b62d11a56b558c
mbouziane <mohamed.bouziane@gmail.com>
2020-12-21 20:08:22 +0100

    fix imports

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,30 +1,29 @@
 package main
 
 import (
-	"google.golang.org/grpc"
+	"fmt"
 	"log"
 	"net"
-	"fmt"
-	
+
+	"github.com/go-grpc-course/greet/greetpb"
+
+	"google.golang.org/grpc"
 )
+
 type server struct{}
+
 func main() {
 	fmt.Printf("Hello World")
 
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
-	if err != nil{
+	if err != nil {
 		log.Fatalf("failed to run server: %v", err)
 	}
 
-	s:= grpc.NewServer()
-
+	s := grpc.NewServer()
 	greetpb.RegisterGreetServiceServer(s, &server{})
-
-	if err := s.Serve(lis); err!= nil{
+	if err := s.Serve(lis); err != nil {
 		log.Fatalf("error : %v", err)
 	}
-
-
-
 
 }
